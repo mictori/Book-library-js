9bc0c0d1b94c708de6d3bc624441f5da76841cf9
mictori <mmictori@gmail.com>
2024-09-25 13:28:57 +0200

    Modal form; get input value, push into library

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-const myLibrary = [
-	{
-		title: 'Harry Potter and the Chamber of Secrets',
-		author: 'J. K. Rowling',
-		pages: 251,
-		isRead: false,
-		id: 1,
-	},
-	{
-		title: 'Harry Potter and the Chamber of Secrets',
-		author: 'J. K. Rowling',
-		pages: 251,
-		isRead: false,
-		id: 1,
-	},
-];
+const myLibrary = [];
 
 const booksContainer = document.querySelector('.book-container');
+const newBookBtn = document.querySelector('.new-book-btn');
+const modal = document.querySelector('.modal');
+const submitBtn = document.querySelector('.submit-btn');
+const modalForm = document.querySelector('.modal-form');
 
 function Book(title, author, pages, isRead) {
 	this.title = title;
@@ -25,15 +14,35 @@ function Book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary() {
+	const title = document.querySelector('#book-title').value;
+	const author = document.querySelector('#book-author').value;
+	const pages = document.querySelector('#book-pages').value;
+	const isRead = Boolean(
+		document.querySelector('input[name="is-read"]:checked').value
+	);
+
+	const newBook = new Book(title, author, pages, isRead);
+	myLibrary.unshift(newBook);
+
+	modal.close();
+	modalForm.reset();
+
 	booksContainer.innerHTML = myLibrary
 		.map((book) => {
-			return `<section class='book'>
+			return `<section class='book' id='${myLibrary.indexOf(book)}'>
 	                <h3>${book.title}</h3>
-                    <p>${book.author}</p>
-                    <p>${book.pages}</p>
+                    <p>Author: ${book.author}</p>
+                    <p>Pages: ${book.pages}</p>
+                    <div class='read-label'>${
+						book.isRead ? 'Read' : 'Unread'
+					}</div>
 	            </section>`;
 		})
 		.join(' ');
 }
 
-addBookToLibrary();
+newBookBtn.addEventListener('click', () => {
+	modal.showModal();
+});
+
+submitBtn.addEventListener('click', addBookToLibrary);
